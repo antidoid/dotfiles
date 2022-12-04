2d3e837deeff96b86a3361cee4bf2e33f341f748
Piyush Singh <singhpiyush998@gmail.com>
2022-12-04 23:12:32 +0530

    Add: Vertical Movements Keymaps

--- nvim/lua/antid/remap.lua
+++ nvim/lua/antid/remap.lua
@@ -1,6 +1,12 @@
 local nnoremap = require('antid.keymap').nnoremap
 local inoremap = require("antid.keymap").inoremap
 
-
+-- Normal Mode Remaps
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+nnoremap("<C-d>", "<C-d>zz")
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("n", "nzzzv")
+nnoremap("N", "Nzzzv")
+
+-- Insert mode Remaps
 inoremap("jk", "<esc>")
