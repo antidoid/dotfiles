d308709a8cffab503d3a9f49ad7f8e230b1dbbe3
Piyush Singh <singhpiyush998@gmail.com>
2023-06-14 21:04:09 +0530

    Add: flake.nix

---
+++ nix/flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixgl.url = "github:guibou/nixGL";
+  };
+
+  outputs = { nixpkgs, home-manager, nixgl, ... }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ nixgl.overlay ];
+      };
+    in {
+      homeConfigurations."antid" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
