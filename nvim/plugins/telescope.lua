42c0388ba373b8f4040113dd72f47d4d8fc36d6a
antidoid <singhpiyush998@gmail.com>
2023-07-25 20:22:05 +0530

    Fix: Telescope to not display node_modules 🫡

---
+++ nvim/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  config = function()
+    require("telescope").setup {
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules",
+        },
+      },
+    }
+  end,
+}
