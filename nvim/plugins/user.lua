13c0b39afa38cc4bf9bdd80c51295417ae378577
antidoid <singhpiyush998@gmail.com>
2023-12-21 17:16:02 +0530

    Add: tmux configs

--- nvim/plugins/user.lua
+++ nvim/plugins/user.lua
@@ -1,15 +1,7 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  --
+  -- Same keybinds to switch b/w tmux panes and vim panes
+  "christoomey/vim-tmux-navigator",
+
   -- TODO comments
   {
     "folke/todo-comments.nvim",
@@ -55,7 +47,7 @@ return {
   -- Nvim surround
   {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "*",
     event = "VeryLazy",
     config = function() require("nvim-surround").setup {} end,
   },
