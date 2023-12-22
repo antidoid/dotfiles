6dd562be2ccf435ee3c73cd2fd310a84a8049439
antidoid <singhpiyush998@gmail.com>
2023-12-22 19:23:20 +0530

    Update: Theme and keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,68 +7,16 @@ end
 
 config = {
 	hide_mouse_cursor_when_typing = false,
-	color_scheme = "UnderTheSea",
+	color_scheme = "Rasi (terminal.sexy)",
 	font = wezterm.font_with_fallback({
 		"FiraCode Nerd Font Mono Med",
-		"JetBrains Mono"
+		"JetBrains Mono",
 	}),
 	window_decorations = "RESIZE",
 	initial_rows = 42,
-	initial_cols = 150,
+	initial_cols = 140,
 	hide_tab_bar_if_only_one_tab = true,
 	default_prog = { "fish" },
-	keys = {
-		{
-			key = "|",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "\\",
-			mods = "CTRL",
-			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "h",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "l",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "k",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.ActivatePaneDirection("Up"),
-		},
-		{
-			key = "j",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.ActivatePaneDirection("Down"),
-		},
-		{
-			key = "h",
-			mods = "CTRL|ALT|SHIFT",
-			action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-		},
-		{
-			key = "j",
-			mods = "CTRL|ALT|SHIFT",
-			action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-		},
-		{
-			key = "k",
-			mods = "CTRL|ALT|SHIFT",
-			action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-		},
-		{
-			key = "l",
-			mods = "CTRL|ALT|SHIFT",
-			action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-		},
-	},
 }
 
 return config
