6ca3f447d3bc1d17061f7c11e8e9044571ec5153
antidoid <singhpiyush998@gmail.com>
2023-12-26 16:29:18 +0530

    Add: ability to show file name in status line

--- nvim/plugins/user.lua
+++ nvim/plugins/user.lua
@@ -56,7 +56,25 @@ return {
   {
     "rebelot/heirline.nvim",
     opts = function(_, opts)
+      local status = require "astronvim.utils.status"
+
       opts.tabline = nil -- remove tabline
+
+      opts.statusline = {
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+        status.component.git_branch(),
+        status.component.file_info {}, -- Show the file name
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.nav(),
+      }
+
       return opts
     end,
   },
