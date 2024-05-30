43abc643689aba2dd1d7158fc05efc78e5869af4
antidoid <singhpiyush998@gmail.com>
2024-05-30 12:22:18 +0530

    UPDAATE: neovim config

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "max397574/better-escape.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { " akinsho/toggleterm.nvim", enabled = false },
+}
